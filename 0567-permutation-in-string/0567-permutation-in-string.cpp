42f88013ef73cdfca83892d9eb0da9ab16479c9e
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2025-07-06 14:46:00 +0530

    Time: 0 ms (100%), Space: 10 MB (67.78%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
-    bool isFreqSame(int freq1[],int freq2[]){
-        for(int i=0;i<26;i++){
-            if(freq1[i] != freq2[i]){
-                return false;
-            }
+    bool checkInclusion(string s1, string s2) {
+        if (s1.length() > s2.length()) return false;
+
+        int freq1[26] = {0};
+        int freq2[26] = {0};
+        int windowSize = s1.length();
+
+        // Count frequency of characters in s1 and first window of s2
+        for (int i = 0; i < windowSize; i++) {
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
+        }
+
+        // Sliding window over s2
+        for (int i = windowSize; i < s2.length(); i++) {
+            if (equal(freq1, freq2)) return true;
+
+            // Slide the window: remove the char going out, add the char coming in
+            freq2[s2[i] - 'a']++;
+            freq2[s2[i - windowSize] - 'a']--;
+        }
+
+        // Final check for the last window
+        return equal(freq1, freq2);
+    }
+
+    // Frequency comparison function (inline)
+    bool equal(int a[26], int b[26]) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) return false;
         }
         return true;
-    }
-    bool checkInclusion(string s1, string s2) {
-        int freq[26]={0};
-        for(int i=0;i<s1.length();i++){
-            freq[s1[i]-'a']++;
-        }        
-        int windSize=s1.length();
-        
-        for(int i=0;i<s2.length();i++){
-            int windIdx =0,idx=i;
-            int windFreq[26]={0};
-            while(windIdx<windSize && idx<s2.length()){
-                windFreq[s2[idx]-'a']++;
-                windIdx++;idx++;
-            }
-            if(isFreqSame(freq,windFreq)){
-                return true;
-            }
-        }
-        return false;
     }
 };
