a3dd7d990348b4348a994e6c1fe3fe330f59d942
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2025-07-30 19:54:41 +0530

    Time: 1263 ms (5%), Space: 0B (100%) - LeetHub

--- 1179-game-play-analysis-i/1179-game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/1179-game-play-analysis-i.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
-SELECT player_id,MIN(event_date) AS first_login
+SELECT
+    player_id,
+    MIN(event_date) As first_login
 FROM Activity
 GROUP BY player_id
