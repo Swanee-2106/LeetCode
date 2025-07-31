b8e664a6f06970ca9849a278a768bc77b4afe9a9
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2025-08-01 02:27:50 +0530

    Time: 0 ms (100%), Space: 20.8 MB (26.04%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int i = 0;i<nums.size();i++){
-            bool isUnique = true;
-            for(int j=0;j<nums.size();j++){
-                if(i!=j && nums[j]==nums[i]){
-                    isUnique=false;
-                    break;
-                }
-            }
-            if(isUnique==true){
-                ans = nums[i];
-            }
+        int ans = 0;
+        for(int val : nums){
+            ans = ans^val;
         }
-    return ans;
+        return ans;
     }
 };
