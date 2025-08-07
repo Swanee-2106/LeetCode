a15839ed52d78c06797272b56180bf383e29f413
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2025-08-08 00:57:57 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.5%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        //finding the pivot
         int pivot=-1;
         int n=nums.size();
         for(int i=n-2;i>=0;i--){
@@ -9,16 +10,19 @@ public:
                 break;
             }
         }
-        if(pivot==-1){
+        if(pivot==-1){//if pivot does not exist inplace reverse
             reverse(nums.begin(),nums.end());
             return;
         }
+        //2nd is reversing the pivot element with the right most elem
         for(int i=n-1;i>pivot;i--){
             if(nums[i]>nums[pivot]){
                 swap(nums[i],nums[pivot]);
                 break;
             }
         }
+        //3rd Reverse the remaining array after pivot i.e. pivot+1 to n-1 to form the smallest element
+        //reverse(nums.begin()+pivot+1,nums.end());
         int i=pivot+1,j=n-1;
         while(i<=j){
             swap(nums[i],nums[j]);
