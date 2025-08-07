5cc133b0f1eb833bac949e9434a8360d5b77fa0d
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2025-08-07 18:19:52 +0530

    Time: 0 ms (100%), Space: 17.5 MB (86.37%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int idx1=-1;int idx2=-1;
-        vector<int> vec;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target && idx1==-1){
-                idx1=i;
-            }
-            if(nums[i]==target){
-                idx2=i;
+        vector<int> vec(2,-1);
+        int st=0,end=nums.size()-1;
+        while(st<=end){//for first occurrence
+            int mid=st+(end-st)/2;
+            if(nums[mid]==target){
+                vec[0]=mid;
+                end=mid-1;//iske pehle koi hai ya nahin ye check karo left side mein
+            }else if(nums[mid]>target){//left search
+                end=mid-1;
+            }else{//right search
+                st=mid+1;
             }
         }
-        vec.push_back(idx1);
-        vec.push_back(idx2);
+        st=0,end=nums.size()-1;
+        while(st<=end){//for last occurrence
+            int mid=st+(end-st)/2;
+            if(nums[mid]==target){
+                vec[1]=mid;
+                st=mid+1;//iske baad koi hai ya nahin ye check karo right side mein
+            }else if(nums[mid]>target){//left search
+                end=mid-1;
+            }else{//right search
+                st=mid+1;
+            }
+        }
         return vec;
     }
 };
