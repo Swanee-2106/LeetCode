a4ab9d29eb56371914ceffc737995cec2bd70274
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2025-08-07 15:01:47 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.59%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long int st=0;long int end=x;long int ans=0;
+        while(st<=end){
+            long int mid=st+(end-st)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            if(mid*mid>x){
+                end=mid-1;
+            }else if(mid*mid<x){
+                ans=mid;
+                st=mid+1;
+            }
+        }
+        return ans;
+    }
+};
