c3830f09a40e6511fe1fdf632d7a2c9d2abf9198
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2025-08-08 11:00:54 +0530

    Time: 285 ms (35.54%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -2,3 +2,10 @@
 SELECT max(salary) AS SecondHighestSalary
 FROM Employee
 WHERE salary NOT IN (SELECT max(salary) FROM Employee)
+#another way
+#SELECT (
+#   SELECT DISTINCT salary
+#    FROM Employee
+#    ORDER BY salary DESC
+#    LIMIT 1 OFFSET 1
+#) AS SecondHighestSalary
