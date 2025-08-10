a491d78d4d8997656dd2164eb058302a1be2665a
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2025-08-10 20:44:01 +0530

    Time: 0 ms (100%), Space: 31.3 MB (45.2%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
+    int binarySearch(vector<int>& arr, int tar,int st,int end){//helper function
+        if(st<=end){
+            int mid=st+(end-st)/2;
+            if(arr[mid]==tar){
+                return mid;
+            }
+            if(arr[mid]<=tar){//second half search
+                return binarySearch(arr,tar,mid+1,end);
+            }else{
+                return binarySearch(arr,tar,st,mid-1);
+            }
+        }
+        return -1;
+    }
     int search(vector<int>& nums, int target) {
-        int st=0;int end=nums.size()-1;
+        /*int st=0;int end=nums.size()-1;
         while(st<=end){
             int mid= st + (end-st)/2;
             if(nums[mid]==target){
@@ -13,6 +27,9 @@ public:
                 st= mid+1;
             }
         }
-        return -1;
+        return -1;*/
+        int st=0;int end=nums.size()-1;
+        return binarySearch(nums,target,st,end);
+
     }
 };
