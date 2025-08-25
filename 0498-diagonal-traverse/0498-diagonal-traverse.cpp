5f2c825796063edb2f80834caca1967c436bad9f
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2025-08-26 00:48:01 +0530

    Time: 4 ms (56.51%), Space: 23 MB (49.52%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -15,6 +15,34 @@ public:
                 }
             }
         }
-        return ans;   
+        return ans;  
+        /*class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        vector<int> ans;
+        int m=mat.size();
+        int n= mat[0].size();
+        for(int d=0;d<m+n-1;d++){
+            if(d%2==0){
+                for(int i=m-1;i>=0;i--){
+                    for(int j=0;j<n;j++){
+                        if(i+j==d){
+                            ans.push_back(mat[i][j]);
+                        }
+                    }
+                }
+            }else{
+                for(int i=0;i<m;i++){
+                    for(int j=n-1;j>=0;j--){
+                        if(i+j==d){
+                            ans.push_back(mat[i][j]);
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};*/ 
     }
 };
