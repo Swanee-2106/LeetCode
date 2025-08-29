ec850e44d734a9974ef7067b9a789994c1e311e1
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2025-08-29 20:24:04 +0530

    Time: 1051 ms (12.03%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,14 @@
 # Write your MySQL query statement below
+/*DELETE FROM Person
+WHERE Id IN (
+    SELECT Id
+    FROM (
+        SELECT Id, ROW_NUMBER() OVER (PARTITION BY Email ORDER BY Id) AS rn
+        FROM Person
+    ) t
+    WHERE rn > 1
+);*/
 DELETE p2
 FROM Person p1
-LEFT JOIN Person p2
-ON p1.email = p2.email AND p2.id>p1.id
+JOIN Person p2
+ON p1.email=p2.email AND p2.id>p1.id;
