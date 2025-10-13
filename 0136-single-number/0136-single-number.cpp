9d9e16c9331ae5de7a31cd8fea08df79627bfbd1
Swanee Prakash <96293378+Swanee-2106@users.noreply.github.com>
2025-10-13 20:14:14 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.25%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int val : nums){
+        //Bitwise operator xor n^n=0 0^n=n
+        int ans=0;
+        for(int val:nums){
             ans = ans^val;
         }
         return ans;
